d5572dc0bc7dbb387a333e03eb1057b776087a61
tukqph20340 <118039480+tukqph20340@users.noreply.github.com>
2023-12-14 22:04:27 +0700

    mainnn

--- src/main/java/com/example/demo/service/Gio_Hang_Chi_Tiet_Service.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.service;
-
-public interface Gio_Hang_Chi_Tiet_Service {
-}
--- src/main/java/com/example/demo/service/impl/Gio_Hang_Chi_Tiet_Serviceimpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.Gio_Hang_Chi_Tiet_Service;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Gio_Hang_Chi_Tiet_Serviceimpl implements Gio_Hang_Chi_Tiet_Service {
-}
